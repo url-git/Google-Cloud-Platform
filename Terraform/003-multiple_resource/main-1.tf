ccb62c6967cf342d17e4df7fbd7aa0fb65bce9ed
Piotr <pmackowka@gmail.com>
2024-11-12 21:28:33 +0100

    003-multiple_resource

---
+++ Terraform/003-multiple_resource/main-1.tf
@@ -0,0 +1,13 @@
+resource local_file cat_res {
+  filename = "cat.txt"
+  content = "I Love Cats"
+}
+
+resource local_file dog_res {
+  filename = "dog.txt"
+  content = "I Love Dogs"
+}
+
+# terraform init
+# terraform plan
+# terraform apply
